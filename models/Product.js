455b2fae3908ac3ea7f55bcac5de08c7c26cb90b
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-31 12:36:06 +0200

    Get functions fixed!

--- models/Product.js
+++ models/Product.js
@@ -33,4 +33,10 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("Product", productSchema);
+//The convention is Document(Schema) name, schma object and then remote colection name
+//It is better to have Document name here as default convention and allow moongoose to create pural of document name remote by default
+//This is to keep the DB clean
+// The reposnsbility of creating and naming collection should be done by moongose in plural is better
+
+//export default mongoose.model("Product", productSchema);
+export default mongoose.model("Product", productSchema, "Products");
---
+++ modules/product/product.controller.js
@@ -0,0 +1,80 @@
+import Product from "../../models/Product.js";
+
+// export const getProducts = async (request, response) => {
+//   try {
+//     // limit = items per page
+//     let { page = 1, limit = 10 } = request.query;
+//     // Page 1 Skip 0
+//     // Page 2 Skip 10
+//     // Page 3 Skip 20
+//     // For page skip (page - 1) * itemsPerPage
+//     let products = await Product.find()
+//       .limit(limit)
+//       .skip((page - 1) * limit)
+//       // Sorting by createdAt in descendant order
+//       .sort({ createdAt: -1 });
+//     return response.send(products);
+//   } catch (error) {
+//     return response.status(500).send("Server Error");
+//   }
+// };
+
+//Basic fetching
+export const getProducts = async (request, response) => {
+  try {
+    let products = await Product.find();
+    return response.send(products);
+  } catch (error) {
+    return response.status(500).send("Server Error");
+  }
+};
+
+export const getProduct = async (request, response) => {
+  let { id } = request.params;
+  // In case you are fetching with id
+  // let product = await Product.findById(id)
+
+  // In case you are fetching with another field like slug for example
+  //This is find one
+  //let product = await Product.findOne({ id });
+  //The alternative is, this is more performant as well
+  let product = await Product.findById(id);
+  if (!product) return response.status(404).send("Product not found");
+  return response.send(product);
+};
+
+export const createProduct = async (request, response) => {
+  try {
+    let { name, description, price } = request.body;
+    let userId = request.headers.authorization;
+    // I check if user is authorized
+    if (!userId) return response.status(401).send("Unauthorized");
+    // I generate slug from title
+    let slug =
+      name.replaceAll(" ", "-").toLowerCase() + "-" + new Date().getTime();
+    // I create the product
+    await Product.create({
+      name,
+      slug,
+      description,
+      price,
+    });
+    response.status(201).send(`Product created successfully`);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send("Server Error");
+  }
+};
+
+export const updateProduct = (request, response) => {
+  let { id } = request.params;
+  let body = request.body;
+  response
+    .status(202)
+    .send(`Updating a product of id: ${id} with data: ${body}`);
+};
+
+export const deleteProduct = (request, response) => {
+  let { id } = request.params;
+  response.status(202).send(`Updating a product of id: ${id}`);
+};
