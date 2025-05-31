4b4b0cb8adb949e74313f25f363dabf664239565
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-31 10:29:12 +0200

    initial connection to database performed

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+//import authRouter from "./routers/auth.router.js";
+//import productRouter from "./routers/product.router.js";
+import { startDatabase } from "./config/db.js";
+
+const app = express();
+
+app.use(express.json());
+
+//app.use("/auth", authRouter);
+//app.use("/products", productRouter);
+
+app.listen(5001, async () => {
+  await startDatabase();
+  console.log("Server is running");
+});
