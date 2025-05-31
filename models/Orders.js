c2f34b9872e170deeb9d64cecb8efde0c76317f2
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-31 11:15:08 +0200

    models generated!

---
+++ models/Orders.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    Product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Order", orderSchema);
---
+++ models/Product.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
