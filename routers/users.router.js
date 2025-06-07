c1a121c75fb0f7bcf891763b10c51d8fd2279f9c
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-07 13:12:20 +0200

    /users authentication has been added!

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllusers } from "../modules/users/users.controller.js";
+import { AuthGuard, RoleGuard } from "../modules/auth/auth.middleware.js";
 
 const router = express.Router();
 
 // router.get("/", getProducts);
 
-router.get("/", getAllusers);
+router.get("/", AuthGuard, RoleGuard("admin"), getAllusers);
 
 export default router;
